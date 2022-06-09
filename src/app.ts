dd7e3183e9094408d3e2c340f4f441fbf5d7b124
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2022-06-09 17:19:26 -0300

    feat: adiciona as rotas e middleware de erro ao app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express from 'express';
-
-// First commit
+import router from './routes';
+import error from './middlewares/error';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/', router);
+
+app.use(error);
 
 export default app;
