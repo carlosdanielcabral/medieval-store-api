61408ae0df588061b54eb363a187b9e54539c3ae
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2022-06-09 16:00:34 -0300

    fix: altera os middlewares de validação do produto

--- src/middlewares/validate.ts
+++ src/middlewares/validate.ts
@@ -1,15 +1,27 @@
 import { RequestHandler } from 'express';
 import Joi from 'joi';
+import HttpStatusCode from '../enums/HttpStatusCode';
 
 export const productName: RequestHandler = (req, res, next) => {
   const { name } = req.query;
 
-  const { error } = Joi.object({
+  const { error: error400 } = Joi.object({
+    name: Joi.required(),
+  })
+    .validate({ name });
+  
+  if (error400) {
+    return next({ code: HttpStatusCode.BadRequest, message: error400.details[0].message });
+  }
+
+  const { error: error422 } = Joi.object({
     name: Joi.string().min(3).required(),
   })
     .validate({ name });
   
-  if (error) return next(error);
+  if (error422) {
+    return next({ code: HttpStatusCode.UnprocessableEntity, message: error422.details[0].message });
+  }
 
   next();
 };
@@ -17,12 +29,23 @@ export const productName: RequestHandler = (req, res, next) => {
 export const productAmount: RequestHandler = (req, res, next) => {
   const { amount } = req.query;
 
-  const { error } = Joi.object({
-    amount: Joi.string().min(3).required(),
+  const { error: error400 } = Joi.object({
+    amount: Joi.required(),
   })
     .validate({ amount });
 
-  if (error) return next(error);
+  if (error400) {
+    return next({ code: HttpStatusCode.BadRequest, message: error400.details[0].message });
+  }
+
+  const { error: error422 } = Joi.object({
+    amount: Joi.required(),
+  })
+    .validate({ amount });
+
+  if (error422) {
+    return next({ code: HttpStatusCode.UnprocessableEntity, message: error422.details[0].message });
+  }
 
   next();
 };
