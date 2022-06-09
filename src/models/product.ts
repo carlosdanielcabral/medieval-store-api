0645085f1c5f82b5e665daeb96d8e1a347a31ea2
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2022-06-09 15:14:32 -0300

    feat: criação do model de produtos

--- src/models/product.ts
+++ src/models/product.ts
@@ -1,3 +1,15 @@
+import { RowDataPacket } from 'mysql2';
+import Product from '../interfaces/Product';
 import connection from './connection';
+import Queries from './queries';
 
+export const getAll = async (): Promise<Product[]> => {
+  const [result] = await connection.execute<RowDataPacket[]>(Queries.GetAllProducts);
+  return result as Product[];
+};
 
+export const save = async (name: string, amount: string): Promise<{ insertedId: number }> => {
+  const [result] = await connection.execute<RowDataPacket[]>(Queries.SaveProduct, [name, amount]);
+  const { insertedId } = result[0];
+  return insertedId;
+};
