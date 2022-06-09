47374acb64d4a23657a8a82c9b07f76c3643d9de
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2022-06-09 18:09:07 -0300

    feat: adiciona as validações de usuário

--- src/middlewares/validate.ts
+++ src/middlewares/validate.ts
@@ -49,3 +49,99 @@ export const productAmount: RequestHandler = (req, res, next) => {
 
   next();
 };
+
+export const userName: RequestHandler = (req, res, next) => {
+  const { username } = req.body;
+
+  const { error: error400 } = Joi.object({
+    username: Joi.required(),
+  })
+    .validate({ username });
+    
+  if (error400) {
+    return next({ code: HttpStatusCode.BadRequest, message: error400.details[0].message });
+  }
+
+  const { error: error422 } = Joi.object({
+    username: Joi.string().min(3).required(),
+  })
+    .validate({ username });
+  
+  if (error422) {
+    return next({ code: HttpStatusCode.UnprocessableEntity, message: error422.details[0].message });
+  }
+
+  next();
+};
+
+export const userClasse: RequestHandler = (req, res, next) => {
+  const { classe } = req.body;
+
+  const { error: error400 } = Joi.object({
+    classe: Joi.required(),
+  })
+    .validate({ classe });
+    
+  if (error400) {
+    return next({ code: HttpStatusCode.BadRequest, message: error400.details[0].message });
+  }
+
+  const { error: error422 } = Joi.object({
+    classe: Joi.string().min(3).required(),
+  })
+    .validate({ classe });
+  
+  if (error422) {
+    return next({ code: HttpStatusCode.UnprocessableEntity, message: error422.details[0].message });
+  }
+
+  next();
+};
+
+export const userLevel: RequestHandler = (req, res, next) => {
+  const { level } = req.body;
+
+  const { error: error400 } = Joi.object({
+    level: Joi.required(),
+  })
+    .validate({ level });
+    
+  if (error400) {
+    return next({ code: HttpStatusCode.BadRequest, message: error400.details[0].message });
+  }
+
+  const { error: error422 } = Joi.object({
+    level: Joi.number().min(1).required(),
+  })
+    .validate({ level });
+  
+  if (error422) {
+    return next({ code: HttpStatusCode.UnprocessableEntity, message: error422.details[0].message });
+  }
+
+  next();
+};
+
+export const userPassword: RequestHandler = (req, res, next) => {
+  const { password } = req.body;
+
+  const { error: error400 } = Joi.object({
+    password: Joi.required(),
+  })
+    .validate({ password });
+    
+  if (error400) {
+    return next({ code: HttpStatusCode.BadRequest, message: error400.details[0].message });
+  }
+
+  const { error: error422 } = Joi.object({
+    password: Joi.string().min(8).required(),
+  })
+    .validate({ password });
+  
+  if (error422) {
+    return next({ code: HttpStatusCode.UnprocessableEntity, message: error422.details[0].message });
+  }
+
+  next();
+};
